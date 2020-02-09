1fd32079cee0664f0cc5c97a3adae154aa58c92d
GitJavaProgramming <455547940@qq.com>
2020-02-09 18:31:56 +0800

    public final class Class<T> implements java.io.Serializable, GenericDeclaration, Type, AnnotatedElement 继承体系之Class

--- src/main/java/org/pp/mybatis/foundationsupportlayer/reflector/proxy/jdk/JdkInterfaceDynamicProxyHandler.java
+++ src/main/java/org/pp/mybatis/foundationsupportlayer/reflector/proxy/jdk/JdkInterfaceDynamicProxyHandler.java
@@ -22,17 +22,25 @@ public class JdkInterfaceDynamicProxyHandler implements InvocationHandler {
             return method.invoke(this, args);
         }
         // wrap,getAnnotation() ops
-        System.out.println(method.getName() + " invoke.");
-        Annotation[] annotations = method.getAnnotations();
-        for (Annotation annotation : annotations) {
-            System.out.println("annotation annotationType: " + annotation.annotationType());
-            if (annotation.annotationType() == Select.class) {
-                Select select = method.getAnnotation(Select.class);
-                BaseExecutor executor = ExecutorFactory.newExecutor();
-                executor.doQuery(select.value());
-//                System.out.println("annotation value: " + select.value());
-            }
+//        System.out.println(method.getName() + " invoke.");
+//        System.out.println(method.isAnnotationPresent(Select.class));
+        if(method.isAnnotationPresent(Select.class)) {
+            Select select = method.getAnnotation(Select.class);
+            BaseExecutor executor = ExecutorFactory.newExecutor();
+            executor.doQuery(select.value());
+        } else {
+            System.out.println(method.getName() + " no Select Annotation.");
         }
+//        Annotation[] annotations = method.getAnnotations();
+//        for (Annotation annotation : annotations) {
+//            System.out.println("annotation annotationType: " + annotation.annotationType());
+//            if (annotation.annotationType() == Select.class) {
+//                Select select = method.getAnnotation(Select.class);
+//                BaseExecutor executor = ExecutorFactory.newExecutor();
+//                executor.doQuery(select.value());
+////                System.out.println("annotation value: " + select.value());
+//            }
+//        }
         // 接口不提供实现，需要自己实现方法处理逻辑
         // MapperMethod--methodCache--sqlCommand,MethodSignature,Executor......
         return null;
